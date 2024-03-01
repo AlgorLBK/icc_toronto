f75d75c967e27c4c8c1da9dbd25a8663709ecc2c
AlgorLBK <algorlombako@gmail.com>
2024-03-01 13:08:00 -0500

    Second

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useRef, useState} from 'react';
 import { motion } from 'framer-motion';
 import emailjs from "@emailjs/browser";
+import './Contact.css';
 export default function Contact() {
 
     useEffect(() => {
@@ -79,21 +80,33 @@ export default function Contact() {
     };
 
   return (
-    <div style={{'backgroundColor': '#70358E'}}>
+    <div>
         <div className='head-home-container'>
             <motion.p variants={present} initial="hidden" animate="visible" exit="exit" id='bv-home' className='present'>CONTACTER</motion.p>
             <motion.p variants={present} initial="hidden" animate="visible" exit="exit" id='icc-head-home' className='present'>IMPACT CENTRE CHRÉTIEN</motion.p>
             <motion.p variants={present} initial="hidden" animate="visible" exit="exit" id='toronto-head-home' className='present'>TORONTO</motion.p>
         </div>
-        <div style={{'display': 'flex', 'flexDirection': 'column', 'justifyContent' : 'center', 'textAlign': 'center'}}>
+        <div style={{'backgroundColor': '#F1F3F4','display': 'flex', 'flexDirection': 'column', 'justifyContent' : 'center', 'textAlign': 'center', 'padding':'30px'}}>
+            <p>L'église se situe au 3875 McNicoll Avenue, Suite 200.</p>
+            <iframe
+                id='map'
+                width="600"
+                height="300"
+                style={{'border':'0', 'margin':'auto'}}
+                loading="lazy"
+                allowfullscreen
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2889.8466251082396!2d-79.28185428456233!3d43.8071913791163!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4d994e1bfcccb%3A0x4c4b83bc3011b1a6!2s3875%20McNicoll%20Ave%2C%20Scarborough%2C%20ON%20M1X%202E1%2C%20Canada!5e0!3m2!1sen!2sus!4v1671544908107!5m2!1sen!2sus">
+            </iframe>
+        </div>
+        <div style={{'backgroundColor': '#70358E','display': 'flex', 'flexDirection': 'column', 'justifyContent' : 'center', 'textAlign': 'center'}}>
             <div style={{'display': 'flex', 'justifyContent':'center', 'padding': '15px 5px 5px 5px', 'alignItems':'center'}} >
-                <p style={{'textAlign':'center', 'width': '80%', 'color': 'white'}}>Salutations ! Pour nous contacter il vous suffit de remplir ce formulaire et nous vous reviendrons le plus tôt possible.</p>
+                <p style={{'textAlign':'center', 'width': '80%', 'color': 'white'}}>Pour nous contacter il vous suffit de remplir ce formulaire et nous vous reviendrons le plus tôt possible.</p>
             </div>
             <div style={{'display': 'flex', 'justifyContent':'center', 'padding': '5px', 'color': 'white'}} >
                 <p style={{'textAlign':'center', 'width': '80%'}}>Sentez-vous à l'aise, à très bientôt !</p>
             </div>
         </div>
-        <div>
+          <div style={{ 'backgroundColor': '#70358E' }}>
             <form ref={formRef} onSubmit={handleSubmit} id="contactForm" action="#" method="post">
                 <label for="firstName">Prénom :</label>
                 <input onChange={handleChange} value={form.firstName} type="text" id="firstName" name="firstName" required/>
